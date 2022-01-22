6a55b2c49791e5d1b310f93d5d6a7315e17ce90a
josmagalhaes <zemlm_10@hotmail.com>
2022-01-22 18:24:15 +0000

    Fix Connection

--- public/index.js
+++ public/index.js
@@ -1,7 +1,26 @@
+const serverIp      = '10.0.0.23';
+const serverPort    = '3000';
+const local         = true;   // true if running locally, false
+                              // if running on remote server
+
+// Global variables here. ---->
+
+// <----
+
+function preload() {
+  setupClient();
+}
+
 function setup() {
   setuplogger();
   console.log('Initializing...');
   createCanvas(windowWidth, windowHeight);
+
+  sendData('playerColor', { 
+    r: 255,
+    g: 255,
+    b: 255
+  });
 }
 
 function draw() {
@@ -17,3 +36,21 @@ window.addEventListener('devicemotion', function (ev) {
   console.info(ev.acceleration.y, ev.rotationRate.gamma);
   console.log(ev.acceleration.y);
 })
+
+function onReceiveData (data) {
+  // Input data processing here. --->
+
+  if (data.type === 'timestamp') {
+    print(data.timestamp);
+  }
+
+  // <----
+
+  /* Example:
+     if (data.type === 'myDataType') {
+       processMyData(data);
+     }
+
+     Use `data.type` to get the message type sent by host.
+  */
+}
