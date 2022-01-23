9bd6d7b74d4d97681ab7dfc0f96c406fd31cc204
Luis Barrancos <luis.b.barrancos@gmail.com>
2022-01-23 12:27:31 +0000

    WIP

--- public/host.js
+++ public/host.js
@@ -43,18 +43,17 @@ function draw() {
   if (isHostConnected(display = true)) {
     // Display server address
     //displayAddress();
+    displayCustomAddress(color(255, 180), 12, 10, screen_height - 14);
+    tagDiv.html(qr_img);
   }
-
-  displayCustomAddress(color(255, 180), 12, 10, screen_height - 14);
-  tagDiv.html(qr_img);
-
 }
 
 function onClientConnect(data) {
   // Client connect logic here. --->
   console.log(data.id + ' has connected.');
 
-  if (!game.checkId(data.id)) {
+  if (!game.checkId(data.id))
+  {
     game.add(data.id,
       random(0.25 * width, 0.75 * width),
       random(0.25 * height, 0.75 * height),
@@ -65,13 +64,13 @@ function onClientConnect(data) {
   // <----
 }
 
-function onClientDisconnect(data) {
+function onClientDisconnect(data)
+{
   // Client disconnect logic here. --->
-
-  if (game.checkId(data.id)) {
-    game.remove(data.id);
+  if (game.checkId(data.id))
+  {
+        game.remove(data.id);
   }
-
   // <----
 }
 
@@ -108,12 +107,17 @@ function onReceiveData(data) {
   // Input data processing here. --->
   console.log(data);
 
-  if (data.type === 'joystick') {
-    processJoystick(data);
-  } else if (data.type === 'button') {
-    processButton(data);
-  } else if (data.type === 'playerColor') {
-    game.setColor(data.id, data.r * 255, data.g * 255, data.b * 255);
+  if (data.type === 'joystick')
+  {
+        processJoystick(data);
+  }
+  else if (data.type === 'button')
+  {
+        processButton(data);
+  }
+  else if (data.type === 'playerColor')
+  {
+        game.setColor(data.id, data.r * 255, data.g * 255, data.b * 255);
   }
 
   // <----
@@ -129,34 +133,26 @@ function onReceiveData(data) {
 
 // This is included for testing purposes to demonstrate that
 // messages can be sent from a host back to all connected clients
-function mousePressed() {
-  sendData('timestamp', {
-    timestamp: millis()
-  });
+function mousePressed()
+{
+    console.log("Mouse pressed: sending timestamp millis() to client.")
+    sendData("timestamp", {
+        timestamp: millis()
+    });
 }
 
 ////////////
 // Input processing
-function processJoystick(data) {
-
-  game.setVelocity(data.id, data.joystickX * velScale, -data.joystickY * velScale);
-
-  if (debug) {
-    console.log(data.id + ': {' +
-      data.joystickX + ',' +
-      data.joystickY + '}');
-  }
+function processJoystick(data)
+{
+    fill(0, 255, 0);
+    text("process joystick data", width / 2, height / 2);
 }
 
-function processButton(data) {
-  game.players[data.id].val = data.button;
-
-  game.createRipple(data.id, 300, 1000);
-
-  if (debug) {
-    console.log(data.id + ': ' +
-      data.button);
-  }
+function processButton(data)
+{
+    fill(255, 200, 0);
+    text("process button");
 }
 
 ////////////
@@ -169,37 +165,18 @@ class Game {
     this.players = {};
     this.numPlayers = 0;
     this.id = 0;
-    this.colliders = new Group();
-    this.ripples = new Ripples();
   }
 
   add(id, x, y, w, h) {
-    this.players[id] = createSprite(x, y, w, h);
     this.players[id].id = "p" + this.id;
-    this.players[id].setCollider("rectangle", 0, 0, w, h);
     this.players[id].color = color(255, 255, 255);
-    this.players[id].shapeColor = color(255, 255, 255);
-    this.players[id].scale = 1;
-    this.players[id].mass = 1;
-    this.colliders.add(this.players[id]);
     print(this.players[id].id + " added.");
     this.id++;
     this.numPlayers++;
   }
 
   draw() {
-    this.checkBounds();
-    this.ripples.draw();
-    drawSprites();
-  }
-
-  createRipple(id, r, duration) {
-    this.ripples.add(
-      this.players[id].position.x,
-      this.players[id].position.y,
-      r,
-      duration,
-      this.players[id].color);
+      ;
   }
 
   setColor(id, r, g, b) {
@@ -240,95 +217,8 @@ class Game {
 
     pop();
   }
-
-  setVelocity(id, velx, vely) {
-    this.players[id].velocity.x = velx;
-    this.players[id].velocity.y = vely;
-  }
-
-  checkBounds() {
-    for (let id in this.players) {
-
-      if (this.players[id].position.x < 0) {
-        this.players[id].position.x = this.w - 1;
-      }
-
-      if (this.players[id].position.x > this.w) {
-        this.players[id].position.x = 1;
-      }
-
-      if (this.players[id].position.y < 0) {
-        this.players[id].position.y = this.h - 1;
-      }
-
-      if (this.players[id].position.y > this.h) {
-        this.players[id].position.y = 1;
-      }
-    }
-  }
 }
 
-// A simple pair of classes for generating ripples
-class Ripples {
-  constructor() {
-    this.ripples = [];
-  }
-
-  add(x, y, r, duration, rcolor) {
-    this.ripples.push(new Ripple(x, y, r, duration, rcolor));
-  }
-
-  draw() {
-    for (let i = 0; i < this.ripples.length; i++) {
-      // Draw each ripple in the array
-      if (this.ripples[i].draw()) {
-        // If the ripple is finished (returns true), remove it
-        this.ripples.splice(i, 1);
-      }
-    }
-  }
-}
-
-class Ripple {
-  constructor(x, y, r, duration, rcolor) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-
-    // If rcolor is not defined, default to white
-    if (rcolor == null) {
-      rcolor = color(255);
-    }
-
-    this.stroke = rcolor;
-    this.strokeWeight = 3;
-
-    this.duration = duration; // in milliseconds
-    this.startTime = millis();
-    this.endTime = this.startTime + this.duration;
-  }
-
-  draw() {
-    let progress = (this.endTime - millis()) / this.duration;
-    let r = this.r * (1 - progress);
-
-    push();
-    stroke(red(this.stroke),
-      green(this.stroke),
-      blue(this.stroke),
-      255 * progress);
-    strokeWeight(this.strokeWeight);
-    fill(0, 0);
-    ellipse(this.x, this.y, r);
-    pop();
-
-    if (millis() > this.endTime) {
-      return true;
-    }
-
-    return false;
-  }
-}
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
--- public/index.js
+++ public/index.js
@@ -8,6 +8,12 @@ const local = true; // true if running locally, false
 
 // <----
 
+// motion
+let device_motion_value = 0;
+
+let rotx = 0, roty = 0, rotz = 0;
+let accx = 0, accy = 0, accz = 0;
+
 function preload() {
   setupClient();
 }
@@ -31,9 +37,38 @@ function windowResized() {
 function draw() {
   background(0);
   //displayAddress();
+
+  fill(255);
+  textAlign(CENTER, CENTER);
+
+  device_motion_value = constrain(device_motion_value - 2, 0, 200);
+
+  if (device_motion_value > 10)
+  {
+    fill(255, 0, 0);
+    text("Moving device", width / 2, height / 2);
+  }
+  else
+  {
+    fill(0, 100, 255);
+    text("Device is relaxed", width / 2, height / 2);
+  }
+
+  // rotation X, Y, Z from device
+  // ATTENTION: apparently one MUST call in Z,X,Y order (!?!) or
+  //            expect undefined behaviour (... i don't wanna know)
+  //
+  if (rotz != 0 || rotx != 0 || roty != 0)
+  {
+    text(`Z rotation = ${rotz}, X rotation${rotx}, Y rotation${roty}`);
+  }
+  if (accx != 0 || accy != 0 || accz != 0)
+  {
+    text(`Acceleration X = ${accx}, Y = ${accy}, Z = ${accz}`);
+  }
 }
 
-
+/*
 window.addEventListener('deviceorientation', function (ev) {
   console.info(ev.alpha, ev.beta, ev.gamma);
   console.log(ev.alpha);
@@ -48,6 +83,7 @@ window.addEventListener('devicemotion', function (ev) {
   console.info(ev.acceleration.y, ev.rotationRate.gamma);
   console.log(ev.acceleration.y);
 })
+*/
 
 function onReceiveData(data) {
   // Input data processing here. --->
@@ -55,4 +91,24 @@ function onReceiveData(data) {
   if (data.type === 'timestamp') {
     print(data.timestamp);
   }
+}
+
+// called every time the user touches screen or clicks
+function touchMoved()
+{
+  console.log(`Touched at X = ${mouseX}, Y = ${mouseY}`);
+}
+
+// default threshold of 0.5 for device motion on X,Y,Z
+function deviceMoved()
+{
+  device_motion_value = constrain(device_motion_value + 5, 0, 255);
+
+  accx = map(accelerationX, -90, 90, 0.0, 1.0);
+  accy = map(accelerationY, -90, 90, 0.0, 1.0);
+  accz = map(accelerationZ, -90, 90, 0.0, 1.0);
+
+  rotz = constrain(rotationZ, -TWO_PI, TWO_PI);
+  rotx = constrain(rotationX, -TWO_PI, TWO_PI);
+  roty = constrain(rotationY, -TWO_PI, TWO_PI);
 }
